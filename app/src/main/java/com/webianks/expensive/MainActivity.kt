a786da63f38fb03d542ffcb3d9a13f7bca981e78
webianks <webianks@gmail.com>
2019-05-13 18:21:10 +0530

    DatePicker added.

--- app/src/main/java/com/webianks/expensive/MainActivity.kt
+++ app/src/main/java/com/webianks/expensive/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.webianks.expensive
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dateEt: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val background: ImageView = findViewById(R.id.background)
-
-        /*Glide.with(this)
-            .load("https://source.unsplash.com/user/erondu/1300x600")
-            .into(background)
-*/
+        dateEt = findViewById(R.id.date_et)
+        dateEt.setOnClickListener { showDatePickerDialog() }
 
     }
+
+    private fun showDatePickerDialog() {
+        val newFragment: DialogFragment = DatePickerFragment
+        instance = this@MainActivity
+        newFragment.show(supportFragmentManager, "datePicker")
+    }
+
+   companion object DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+        lateinit var instance : MainActivity
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val c = Calendar.getInstance()
+            val year: Int = c.get(Calendar.YEAR)
+            val month: Int = c.get(Calendar.MONTH)
+            val day: Int = c.get(Calendar.DAY_OF_MONTH)
+            val dialog = DatePickerDialog(activity, this, year, month, day)
+            dialog.datePicker.maxDate = c.timeInMillis
+            return dialog
+        }
+
+        override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+           instance.dateEt.setText("$dayOfMonth-${month+1}-$year")
+        }
+    }
+
 }
+
